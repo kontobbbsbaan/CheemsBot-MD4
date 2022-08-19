bb3b7e1e5b51d20c5dfac927569cfb660199f6c4
Kian <108917081+kontobbbsbaan@users.noreply.github.com>
2022-08-19 12:52:33 +0700

    Update xeon.js

--- xeon.js
+++ xeon.js
@@ -172,7 +172,7 @@ XeonLft = await getBuffer(`https://hardianto.xyz/api/goodbye3?profile=${encodeUR
                 const xtime = moment.tz('Asia/Kolkata').format('HH:mm:ss')
 	            const xdate = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
 	            const xmembers = metadata.participants.length
-                let unicorndoc = {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "916909137213-1604595598@g.us"}, "message": {orderMessage: {itemCount: 9999999999999999999999999999999999999999,status: 200, thumbnail: XeonWlcm, surface: 200, message: ` `, orderTitle: 'xeon', sellerJid: '0@s.whatsapp.net'}}, contextInfo: {"forwardingScore":999,"isForwarded":true},sendEphemeral: true}
+                let unicorndoc = {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "916909137213-1604595598@g.us"}, "message": {orderMessage: {itemCount: 85736056578,status: 200, thumbnail: XeonWlcm, surface: 200, message: ` `, orderTitle: 'xeon', sellerJid: '0@s.whatsapp.net'}}, contextInfo: {"forwardingScore":999,"isForwarded":true},sendEphemeral: true}
                 xeonbody = `
 👋 Halo beban @${xeonName.split("@")[0]}\n💐Welcome To ${metadata.subject}`
       //if you copy the code value,
@@ -208,7 +208,7 @@ XeonBotInc.sendMessage(anu.id, buttonMessage, {quoted:unicorndoc})
 	                const xeondate = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
                 	let xeonName = num
                     const xeonmembers = metadata.participants.length
-                    let unicorndoc = {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "916909137213-1604595598@g.us"}, "message": {orderMessage: {itemCount: 9999999999999999999999999999999999999999,status: 200, thumbnail: xeonbuffer, surface: 200, message: ` `, orderTitle: 'xeon', sellerJid: '0@s.whatsapp.net'}}, contextInfo: {"forwardingScore":999,"isForwarded":true},sendEphemeral: true}
+                    let unicorndoc = {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "916909137213-1604595598@g.us"}, "message": {orderMessage: {itemCount: 85736056578,status: 200, thumbnail: xeonbuffer, surface: 200, message: ` `, orderTitle: 'xeon', sellerJid: '0@s.whatsapp.net'}}, contextInfo: {"forwardingScore":999,"isForwarded":true},sendEphemeral: true}
                     xeonbody = `
 👋 Bye beban @${xeonName.split("@")[0]}\n💐 Keluar Dari ${metadata.subject}`
       //if you copy the code value,
