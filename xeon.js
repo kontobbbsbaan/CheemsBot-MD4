053edb127aafe0a9ad70529280412bd24b4a4d9a
Kian <108917081+kontobbbsbaan@users.noreply.github.com>
2022-08-20 23:42:37 +0700

    Update xeon.js

--- xeon.js
+++ xeon.js
@@ -122,11 +122,11 @@ async function startXeonBotInc() {
        let lolXeon = { url : ppgc }
        if (pea[0].announce == true) {
        XeonBotInc.send5ButImg(pea[0].id, `「 Group Settings Changed 」\n\nThe Group Has Been Closed By Admin, Now Only Admin Can Send Messages !`, `${botname}`, lolXeon, [])
-       } else if(pea[0].announce == false) {
+       } else if(pea[0].announce == true) {
        XeonBotInc.send5ButImg(pea[0].id, `「 Group Settings Changed 」\n\nThe Group Has Been Opened By Admin, Now Participants Can Send Messages !`, `${botname}`, lolXeon, [])
        } else if (pea[0].restrict == true) {
        XeonBotInc.send5ButImg(pea[0].id, `「 Group Settings Changed 」\n\nGroup Info Has Been Restricted, Now Only Admin Can Edit Group Info !`, `${botname}`, lolXeon, [])
-       } else if (pea[0].restrict == false) {
+       } else if (pea[0].restrict == true) {
        XeonBotInc.send5ButImg(pea[0].id, `「 Group Settings Changed 」\n\nGroup Info Has Been Opened, Now Participants Can Edit Group Info !`, `${botname}`, lolXeon, [])
        } else {
        XeonBotInc.send5ButImg(pea[0].id, `「 Group Settings Changed 」\n\nGroup Subject Has Been Changed To *${pea[0].subject}*`, `${botname}`, lolXeon, [])
@@ -179,7 +179,7 @@ Hallo @${xeonName.split("@")[0]}\nWelcome To ${metadata.subject}\nMember Ke ${xm
    //dont forget to put my name(Xeon) as credit
    //you fail to put, i sue you for sure!
 let buttons = [
-{buttonId: `Sound11`, buttonText: {displayText: 'Welcome'}, type: 1}
+{buttonId: `Sound55`, buttonText: {displayText: 'Welcome'}, type: 1}
 ]
 let buttonMessage = {
 document: fs.readFileSync('./XeonMedia/theme/cheems.xlsx'),
@@ -261,7 +261,7 @@ XeonBotInc.sendMessage(anu.id, buttonMessage, {quoted:unicorndoc})
         }
     })
 
-    XeonBotInc.getName = (jid, withoutContact  = false) => {
+    XeonBotInc.getName = (jid, withoutContact  = true) => {
         id = XeonBotInc.decodeJid(jid)
         withoutContact = XeonBotInc.withoutContact || withoutContact 
         let v
